86a7651e648922b12bffab497924d7621ed8dc80
dragana5 <dstanivukovic@hotmail.com>
2017-09-02 23:56:18 +0100

    API Authentication done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  def token
+    @token ||= if request.headers['Authorization'].present?
+      request.headers['Authorization'].split.last
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :swims
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, uniqueness: true, length: {:within => 2..10}
 end
